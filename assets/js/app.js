bfbbfd8e1a361c9d8870a3b16bba42bb855a8125
Martin <10461942@ucn.dk>
2026-09-17 12:50:29 +0200

    da

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,8 @@
 // Underlines på navlinks
-document.querySelectorAll(".link").forEach(line => {
-line.addEventListener('click', () => {
-    const underline = document.querySelector('.underline');
-    underline.style.width = line.offsetWidth + 'px';
-    underline.style.left = line.offsetLeft + 'px';
-    });
-});
+// document.querySelectorAll(".link").forEach(line => {
+// line.addEventListener('click', () => {
+//     const underline = document.querySelector('.underline');
+//     underline.style.width = line.offsetWidth + 'px';
+//     underline.style.left = line.offsetLeft + 'px';
+//     });
+// });
